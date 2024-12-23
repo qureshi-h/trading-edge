741410890edf517388602339cc3631b87b27a89d
Hamza <hamza.ahmedqureshi@hotmail.com>
2024-12-23 12:30:08 +1100

    redirect https to http

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,8 +1,8 @@
-// const API_URL = process.env.NEXT_PUBLIC_API_URL;
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
-// if (!API_URL) {
-//     throw new Error('NEXT_PUBLIC_API_URL is not defined in the environment variables.');
-// }
+if (!API_URL) {
+    throw new Error('NEXT_PUBLIC_API_URL is not defined in the environment variables.');
+}
 
 interface ApiResponse<T = unknown> {
     data: T;
@@ -15,18 +15,10 @@ interface ApiParams {
 
 export const api = {
     get: async <T>(endpoint: string, params: ApiParams = {}): Promise<ApiResponse<T>> => {
-        // const url = new URL(endpoint, API_URL);
-        // Object.keys(params).forEach((key) => url.searchParams.append(key, String(params[key])));
+        const url = new URL(endpoint, API_URL);
+        Object.keys(params).forEach((key) => url.searchParams.append(key, String(params[key])));
 
-        // const response = await fetch(url.toString(), {
-        //     method: 'GET',
-        //     headers: {
-        //         'Content-Type': 'application/json',
-        //     },
-        // });
-        console.log(endpoint, params);
-
-        const response = await fetch('/api/stocks/all', {
+        const response = await fetch(url.toString(), {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
@@ -38,20 +30,14 @@ export const api = {
     },
 
     post: async <T>(endpoint: string, body: object): Promise<ApiResponse<T>> => {
-        // const response = await fetch(new URL(endpoint, API_URL).toString(), {
-        //     method: 'POST',
-        //     headers: {
-        //         'Content-Type': 'application/json',
-        //     },
-        //     body: JSON.stringify(body),
-        // });
-        const response = await fetch(endpoint, {
+        const response = await fetch(new URL(endpoint, API_URL).toString(), {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify(body),
         });
+
         const data = await response.json();
         return { data, status: response.status };
     },
