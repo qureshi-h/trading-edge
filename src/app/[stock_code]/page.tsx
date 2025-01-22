b51176fc3fb61638297501290c40ceb9b871fe9a
Hamza <hamza.ahmedqureshi@hotmail.com>
2025-01-23 00:52:37 +1100

    fixed next day error

--- src/app/[stock_code]/page.tsx
+++ src/app/[stock_code]/page.tsx
@@ -10,11 +10,23 @@ import { fetchStockAnalysis } from '@/utils/api/analysis';
 import { Stock, StockAnalysis } from '@/types/stocks';
 import { getDatesExcludingWeekends } from '@/utils/dates';
 
+const getLastAnalysis = async (stock_code: string) => {
+    for (const currentDate of getDatesExcludingWeekends(7)) {
+        const stockAnalysis: StockAnalysis | null = await fetchStockAnalysis(
+            stock_code,
+            currentDate,
+        );
+        if (stockAnalysis !== null) {
+            return { currentDate, stockAnalysis };
+        }
+    }
+    return { currentDate: getDatesExcludingWeekends(1)[0], stockAnalysis: null };
+};
+
 const Page = async ({ params }: { params: Promise<{ stock_code: string }> }) => {
     const { stock_code } = await params;
     const stockInfo: Stock | null = await fetchStock(stock_code.toUpperCase());
-    const currentDate = getDatesExcludingWeekends(1)[0];
-    const stockAnalysis: StockAnalysis | null = await fetchStockAnalysis(stock_code, currentDate);
+    const { currentDate, stockAnalysis } = await getLastAnalysis(stock_code.toUpperCase());
 
     return (
         <PageContainer>
