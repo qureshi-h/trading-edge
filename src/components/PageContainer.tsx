4ccea7ed3edd80a9af354d18747d254b5317cc42
Hamza <hamza.ahmedqureshi@hotmail.com>
2025-03-23 17:22:20 +1100

    refactored header

--- src/components/PageContainer.tsx
+++ src/components/PageContainer.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
+import React, { useMemo, memo } from 'react';
 import Link from 'next/link';
-
-import { Flex } from 'antd';
+import { Flex, FloatButton } from 'antd';
 import { HomeFilled } from '@ant-design/icons';
 import Layout, { Content, Header } from 'antd/es/layout/layout';
 
@@ -15,43 +14,58 @@ interface PageContainerProps {
     backgroundImage?: string;
 }
 
-const PageContainer: React.FC<PageContainerProps> = ({
-    children,
-    style = {},
-    className = 'min-h-screen px-4 md:py-10 lg:py-0',
-    align = 'center',
-    vertical = true,
-    justify = 'center',
-    backgroundImage = `/image.png`,
-}) => {
-    return (
-        <Layout className="min-h-full" hasSider={false}>
-            <Header className="fixed w-fit bg-transparent z-20 ml-10 mt-5 md:ml-10 md:mt-0 px-0">
-                <Link href="/">
-                    <HomeFilled className="!text-white" />
-                </Link>
-            </Header>
+const PageHeader = () => (
+    <FloatButton
+        icon={<HomeFilled />}
+        type="primary"
+        tooltip="Go to Home"
+        aria-label="Go to Home"
+        style={{
+            right: 24,
+            bottom: 24,
+        }}
+        href="/"
+    />
+);
 
-            {/* Main Content Area */}
-            <Content>
-                <Flex
-                    vertical={vertical}
-                    align={align}
-                    justify={justify}
-                    className={className}
-                    style={{
-                        backgroundImage: backgroundImage ? `url('${backgroundImage}')` : undefined,
-                        backgroundSize: 'cover',
-                        backgroundPosition: 'center',
-                        backgroundAttachment: 'fixed',
-                        ...style,
-                    }}
-                >
-                    {children}
-                </Flex>
-            </Content>
-        </Layout>
-    );
-};
+const PageContainer: React.FC<PageContainerProps> = memo(
+    ({
+        children,
+        style = {},
+        className = 'min-h-screen px-4 md:py-10 lg:py-0',
+        align = 'center',
+        vertical = true,
+        justify = 'center',
+        backgroundImage = '/image.png',
+    }) => {
+        const containerStyle = useMemo(
+            () => ({
+                backgroundImage: backgroundImage ? `url('${backgroundImage}')` : undefined,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                backgroundAttachment: 'fixed',
+                ...style,
+            }),
+            [backgroundImage, style],
+        );
+
+        return (
+            <Layout className="min-h-full" hasSider={false}>
+                <PageHeader />
+                <Content>
+                    <Flex
+                        vertical={vertical}
+                        align={align}
+                        justify={justify}
+                        className={className}
+                        style={containerStyle}
+                    >
+                        {children}
+                    </Flex>
+                </Content>
+            </Layout>
+        );
+    },
+);
 
 export default PageContainer;
